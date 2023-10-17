a7d4e6147d011965f680e159356266815a6d24a2
Diego Garcia <diegofgarci@gmail.com>
2023-10-17 17:23:17 -0600

    Hasta DB

--- js/db.js
+++ js/db.js
@@ -8,4 +8,27 @@ req.onupgradeneeded = (e) => {
     db.createObjectStore('users',{
         keyPath: 'id',
     })
+    req.onerror = (e) => {
+        console.log('D-B Error ->', e.target.error);
+    }
+    req.onsuccess = (e) => {
+        let db = e.target.result;
+        let transaction = db.transaction('users', 'readwrite'); 
+    }
+    transaction.onerror = (e) => {
+        console.log('TR - Error ->', e.target.error);
+    }
+    transaction.oncomplete = (e) => {
+        console.log('TR - Done ->', e);
+    }
+    let stored = transaction.objectStore('users');
+    stored.add({
+        id: new Date().toISOString(),
+        username: "Diego",
+        fullname: "Diego Garcia"
+    })
+
+    stored.onsuccess = (e) => {
+        console.log('ST - Success ->', "Agregado Correctamente");
+    }
 }
